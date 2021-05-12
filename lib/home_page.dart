1b192f775e2ca645d451fa3440897b0bd3a86abd
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2021-05-13 00:25:36 +0530

    day2: Scaffold

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    // double days = 30;
+    final String name = "Codepur";
+    // bool isMale = true;
+    // num temprature = 30; // int or double
+    // var x = 1; //Compiler decides
+    // const pi = 3.14;
+    // final a = 10; // can be added item in list but same as const
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
