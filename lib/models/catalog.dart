ce90c71aab46b692aeec497bdaad5b3ca5fbcee2
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2021-05-13 18:41:56 +0530

    day10 :: Model creation

---
+++ lib/models/catalog.dart
@@ -0,0 +1,14 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final products = [
+  Item("Codepor001", "Iphone 12 pro", "Apple ", 900, "#33505a", "https://sadasads")
+]
