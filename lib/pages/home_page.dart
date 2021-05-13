6960d74bde3207ca491f488e191a2f2f6d90eb8f
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2021-05-14 01:25:35 +0530

    day13 :: Json mocks and overriding statefull widget initState

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,37 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/widgets/my_drawer.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // int days = 30;
+  // final String name = "Codepur";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    // print(catalogJson);
+    final decodedJson = jsonDecode(catalogJson);
+    var productsData = decodedJson["products"];
+    print(decodedJson);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // int days = 30;
-    // final String name = "Codepur";
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
