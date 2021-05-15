5a12955365fa842eed4cfe0bd15a64fa7f4f8046
Ashutosh Singh Rawat <singh.ashutoshrawat@gmail.com>
2021-05-15 18:33:54 +0530

    day23 :: Cart Model

---
+++ lib/models/cart.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_catalog/models/catalog.dart';
+
+class CartModel {
+  CartModel _catalog;
+  final List<int> _itemIds = [];
+
+  CartModel get catatog => _catalog;
+
+  set catalog(CartModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  // Get Items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  void add(Item item) {
+    _itemIds.add(item.id);
+  }
+
+  void remove(Item item) {
+    _itemIds.remove(item.id);
+  }
+}
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -14,10 +14,10 @@ class CatalogModel {
   //   )
   // ];
 
-  static Item getById(int id) =>
+  Item getById(int id) =>
       items.firstWhere((element) => element.id == id, orElse: null);
 
-  static Item getByPosition(int pos) => items[pos];
+  Item getByPosition(int pos) => items[pos];
 }
 
 class Item {
